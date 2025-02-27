78b6040000766d5b421ad4526b1530a2b760ba9d
priyalgoti07 <gotipriyal1996@gmail.com>
2025-02-27 10:10:30 +0530

    (404-page): golbal 404-page create and folder wise 404-page create

---
+++ src/app/login/[...not-found]/page.js
@@ -0,0 +1,7 @@
+export default function NotFoundLogig() {
+    return (
+        <div className="m-auto">
+            <h1 className="font-bold text-[25px] text-sky-950 text-center">Not Found Page Specific Login Page 404</h1>
+        </div>
+    )
+}
